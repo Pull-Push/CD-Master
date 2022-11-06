9d916075ed9a8808b8017afdf6008d4aa15e079f
Pull-Push <JeffSokolDev@gmail.com>
2022-11-06 13:16:10 -0500

    upload basketball_dictionary

---
+++ Python/fundamentals/oop/basketball_dictionaries.py
@@ -0,0 +1,79 @@
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32, "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33, "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32, "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+    	"name": "", 
+    	"age":16, 
+    	"position": "P", 
+    	"team": "en"
+    }
+]
+
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32, "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+new_team = []
+
+class Player:
+    
+    def __init__(self, player_info):
+        self.name = player_info["name"]
+        self.age = player_info["age"]
+        self.position = player_info["position"]
+        self.team = player_info["team"]
+    
+    @classmethod
+    def get_team(cls,team_list):
+        pass
+        for x in range(len(team_list)):
+            new_team.append(Player(players[x]))
+
+
+Player.get_team(players)
+
+
+
+# for x in range(len(players)):
+    # new_team.append(Player(players[x]))
+
+
+# print(new_team)
