fb38743233a4ad858c195f2108adac3de66810c4
Jeff Sokol <jeffsokoldev@gmail.com>
2023-03-06 21:00:04 -0500

    upload predictDestructure assignment

---
+++ JavaScript/CoreAssignments/predictDestructure.js
@@ -0,0 +1,73 @@
+
+//! Problem 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+//todo OUTPUT
+//todo one of the cars in the array - not random. the first car in the array
+//todo the second car in the array
+
+
+//! Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+//todo OUTPUT
+//todo Elon both times
+//todo name will not work. not defined since it was defined at otherName
+
+
+//! Problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+//todo OUTPUT
+//todo password will print 12345. hashedPassword will not work. it's not defined.
+
+
+//! Problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+//todo OUTPUT
+//todo False then True
+
+
+//! Problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+//todo OUTPUT
+//todo key = 'value'
+//todo secondkey = the full secondkey array
+//todo secondkey[0] = 1
+//todo willThisWork = 5
